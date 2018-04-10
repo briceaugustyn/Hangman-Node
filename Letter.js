569bf69e08fbb9f0dc22135e0eddd19dcff7115a
Brice Augustyn <baugustyn1000@gmail.com>
2018-04-10 13:32:45 -0700

    first version

---
+++ Letter.js
@@ -0,0 +1,19 @@
+// A string value to store the underlying character for the letter
+// A boolean value that stores whether that letter has been guessed yet
+// A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+// A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+
+
+class Letter {
+    constructor(word,under_char, letter_guess) {
+        this.word = word
+        this.under_char = under_char
+        this.letter_guess = letter_guess
+    }
+    Return_Char() {
+       return underlying-char
+    }
+    Check(character) {
+       
+    }
+}
